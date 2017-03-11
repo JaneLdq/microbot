a3d0bfa26c0c41c9044c5aaa2e6d81adb8da19a9
JaneLdq <janeldq@163.com>
2017-03-11 16:36:07 +0800

    update directory

--- index.js
+++ index.js
@@ -2,3 +2,13 @@
  * index.js
  * 
  */
+ module.exports = {
+ 	Robot: require("./lib/robot"),
+
+ 	Driver: require("./lib/driver"),
+ 	Adaptor: require("./lib/adaptor"),
+ 	
+ 	Service: require("./lib/service"),
+
+ 	Utils: require("./lib/utils"),
+};
