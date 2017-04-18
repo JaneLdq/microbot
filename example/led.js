cb58c359b5d446ec2af8379ebbcf8e987cd7f647
JaneLdq <janeldq@163.com>
2017-04-18 15:42:24 +0800

    add button test demo

--- example/led.js
+++ example/led.js
@@ -3,14 +3,28 @@ let Microbot = require('../index.js');
 let mike = Microbot.robot({
 	name: "Mike",
 	devices: {
-		led: {driver: 'led', connection: 'arduino', pin: 4}
+		led_1: {driver: 'led', connection: 'arduino_A', pin: 9},
+		// led_2: {driver: 'led', connection: 'arduino_B', pin: 10}
+		button: {driver: 'button', connection: 'arduino_A', pin: 2}
 	},
 	connections: {
-		arduino: {adaptor: 'arduino', port: 'COM4'}
+		arduino_A: {adaptor: 'arduino', port: 'COM3'}
+		// arduino_B: {adaptor: 'arduino', port: 'COM3'}
 	},
 	run: function() {
-		setInterval(() => {
-			this.led.toggle();
-		}, 2000);
+		// setInterval(() => {
+		// 	this.led_1.toggle();
+		// }, 2000);
+		// setInterval(() => {
+		// 	this.led_2.toggle();
+		// }, 1000);
+		this.button.on('push', ()=> {
+			 console.log('push');
+			 this.led_1.turnOn();
+		});
+		this.button.on('release', ()=> {
+			 console.log('release');
+			 this.led_1.turnOff();
+		});
 	}
 }).start();
