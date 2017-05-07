02bfb10dd9f36c590a69e9eb954f6c66786e8005
Katharine <sunmengxi19951207@icloud.com>
2017-05-08 00:42:23 +0800

    add i2c-driver to root index.js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 /**
  * index.js
- * 
+ *
  */
- var Master = require('./lib/master');
+var Master = require('./lib/master');
 
- module.exports = {
- 	Robot: require("./lib/robot"),
+module.exports = {
+  Robot: require("./lib/robot"),
 
- 	Driver: require("./lib/driver"),
- 	Adaptor: require("./lib/adaptor"),
- 	
- 	Service: require("./lib/service/service"),
+  Driver: require("./lib/driver"),
+  I2CDriver: require("./lib/i2c-driver"),
+  Adaptor: require("./lib/adaptor"),
 
- 	robot: Master.createRobot
+  Service: require("./lib/service/service"),
+
+  robot: Master.createRobot
 };
